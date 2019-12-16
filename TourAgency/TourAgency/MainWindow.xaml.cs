6718b6ffd2fa75c52cf67b455fbcb9efb97c1e5b
Алексей Чугунов <eky23@yandex.ru>
2019-12-16 19:41:22 +0300

    day 1

---
+++ TourAgency/TourAgency/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TourAgency.Pages;
+
+namespace TourAgency
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFraim.Navigate(new AuthorizationPage());
+        }
+
+        private void MainFraim_ContentRendered(object sender, EventArgs e)
+        {
+            if (MainFraim.CanGoBack)
+            {
+                BackButton.Visibility = Visibility.Visible;
+            } else
+            {
+                BackButton.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFraim.CanGoBack)
+            {
+                MainFraim.GoBack();
+            }
+        }
+    }
+}
---
+++ TourAgency/TourAgency/Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TourAgency.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RegistrationButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RegistrationPage());
+
+        }
+    }
+}
